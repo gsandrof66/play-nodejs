48d9a0d6ceed2fa681846f4d8b64c3e5fb6df2a1
Sandro Strongs <gsandrof66@gmail.com>
2020-06-09 13:36:59 -0500

    with package json

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const http = require('http');
 http.createServer(function (req, res) {
     res.end('<h1>Hello world</h1>');
-}).listen(3003, () => console.log('listen 3003 port'));
+}).listen(3003, () => console.log('listening 3003 port'));
