0ab90fac267e11e17fb0f19a247b35dbcea78d5f
Sandro Strongs <gsandrof66@gmail.com>
2020-06-15 18:00:27 -0500

    new changes at 15-06-2020

---
+++ server.js
@@ -0,0 +1,6 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+const http = require('http');
+http.createServer( (req, res)=> {
+    res.end('<h1>Hello world</h1>');
+}).listen(server_port, server_ip_address,() => console.log('listening 3003 port'));
--- start.js
+++
@@ -1,4 +0,0 @@
-const http = require('http');
-http.createServer(function (req, res) {
-    res.end('<h1>Hello world</h1>');
-}).listen(3003, () => console.log('listening 3003 port'));
